b06a9426c63d4975d193eba96e35cd3a85b13eeb
Darren Truong <128624091+darren-truong@users.noreply.github.com>
2025-06-25 23:29:24 -0700

    Add core components: title, text area, stats, and footer

---
+++ src/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <footer>
+      <small>This website leverages React.</small>
+      <small>How neat!</small>
+    </footer>
+  );
+}
---
+++ src/H1.jsx
@@ -0,0 +1,7 @@
+export default function H1() {
+  return (
+    <h1>
+      Word<span>Wise</span>
+    </h1>
+  );
+}
